ae509538493727150e8ddbe2fd41dd7a88f7b04b
mesut <mesutaygn@gmail.com>
2017-03-01 22:18:27 +0200

    Create pythontestscript.py

---
+++ pythontestscript.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import psycopg2
+import sys
+import time
+
+
+con = None
+
+try:
+
+    con = psycopg2.connect("dbname='postgres' user='postgres' password='123456' host='localhost'");
+    cur = con.cursor();
+    while True:
+     start_time = time.time()
+     cur.execute("INSERT INTO test2(ad,zaman) VALUES('mesut',now())");
+     con.commit()
+     end_time = time.time()
+     print "Yazma suresi %s"%(end_time-start_time),
+except psycopg2.DatabaseError, e:
+    
+    if con:
+        con.rollback()
+    print 'Error %s' % e    
+    sys.exit(1)
+    
+    
+finally:
+    
+    if con:
+        con.close()
